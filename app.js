4f24460c07d0b21966a865f388df3191f3a38671
samamoun <s85Azu2wN)a7dE>
2021-11-30 21:01:24 -0500

    fixed trees animation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/////////////////////// ME FALLING ~~ animated /////////////////////
+
 //bezier object
 const fallPath = {
     //amount of curviness 
@@ -13,10 +15,10 @@ const fallPath = {
         {x:-20, y: 150},
         {x:-10, y: 200},
         {x:0, y: 250},
-        {x:10, y: 350},
-        {x:20, y: 400},
-        {x:10, y: 450},
-        {x:0, y: 450},
+        // {x:10, y: 350},
+        // {x:20, y: 400},
+        // {x:10, y: 450},
+        // {x:0, y: 450},
 
     ]
 };
@@ -50,12 +52,164 @@ const scene = new ScrollMagic.Scene({
     .setTween(tween)
     // .addIndicators()
     //stuck here 
-    .setPin('.animation')
+    .setPin('.animation',400)
     .addTo(controller);
 
 // IN THE FUTURE, should maybe use .each function to
 // loop thruogh each scene and create a new ScrollMagic scene for each one 
 
 
+/////////////////////// Trees /////////////////////
 
-/////////////////////// TWO /////////////////////
+const tree1Path = {
+    curviness: 0.255,
+    values: [
+        {x:0, y: 0},
+        {x:100, y: 0},
+    ]
+};
+
+const tween1 = new TimelineLite();
+
+tween1.add(
+    TweenLite.to(".trees1", 1 , {
+     bezier: tree1Path, 
+     ease: Power1.easeInOut   
+    })
+);
+
+const controller1 = new ScrollMagic.Controller();
+
+const scene1 = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 5000,
+    offset: 200,
+    triggerHook: 0
+} ) 
+    .setTween(tween1)
+    .addTo(controller1);
+
+
+
+/////////////////////// Trees 2 /////////////////////
+
+const tree2Path = {
+    curviness: 0.255,
+    values: [
+        {x:0, y: 0},
+        {x:-100, y: 0},
+    ]
+};
+
+const tween2 = new TimelineLite();
+
+tween2.add(
+    TweenLite.to(".trees2", 1 , {
+     bezier: tree2Path, 
+     ease: Power1.easeInOut   
+    })
+);
+
+const controller2 = new ScrollMagic.Controller();
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 5000,
+    offset: 200,
+    triggerHook: 0
+} ) 
+    .setTween(tween2)
+    .addTo(controller2);
+
+
+/////////////////////// Trees 3 /////////////////////
+
+const tree3Path = {
+    curviness: 0.255,
+    values: [
+        {x:800, y: 50},
+        {x:850, y: 100},
+    ]
+};
+
+const tween3 = new TimelineLite();
+
+tween3.add(
+    TweenLite.to(".trees3", 1 , {
+     bezier: tree3Path, 
+     ease: Power1.easeInOut   
+    })
+);
+
+const controller3 = new ScrollMagic.Controller();
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 5000,
+    offset: 200,
+    triggerHook: 0
+} ) 
+    .setTween(tween3)
+    .addTo(controller3);
+
+
+/////////////////////// Trees 4 /////////////////////
+
+const tree4Path = {
+    curviness: 0.255,
+    values: [
+        {x:-800, y: 50},
+        {x:-700, y: 100},
+    ]
+};
+
+const tween4 = new TimelineLite();
+
+tween4.add(
+    TweenLite.to(".trees4", 1 , {
+     bezier: tree4Path, 
+     ease: Power1.easeInOut   
+    })
+);
+
+const controller4 = new ScrollMagic.Controller();
+
+const scene4 = new ScrollMagic.Scene({
+    triggerElement: '.animation',
+    duration: 5000,
+    offset: 200,
+    triggerHook: 0
+} ) 
+    .setTween(tween4)
+    .addTo(controller4);
+
+
+////////////////// me again ////////////////////////
+
+// const p1 = {
+//     curviness: 0.255,
+//     values: [
+//         {x:0, y: 0},
+//         {x:200, y: 0},
+//     ]
+// };
+
+// const tweenp1 = new TimelineLite();
+
+// tweenp1.add(
+//     TweenLite.to(".p1", 1 , {
+//      bezier: p1, 
+//      ease: Power1.easeInOut   
+//     })
+// );
+
+// const controllerp1 = new ScrollMagic.Controller();
+
+// const scenep1 = new ScrollMagic.Scene({
+//     triggerElement: '.animation',
+//     duration: 5000,
+//     offset: 200,
+//     triggerHook: 0
+// } ) 
+//     .setTween(tweenp1)
+//     .addTo(controllerp1);
