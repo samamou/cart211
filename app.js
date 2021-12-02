4350c43d728174320dac254ae1201f30957b86ac
samamoun <s85Azu2wN)a7dE>
2021-12-02 03:13:12 -0500

    third section added

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ const fallPath = {
     ]
 };
 
-
 //create a tween and animate along this path
 const tween = new TimelineLite();
 
@@ -92,7 +91,6 @@ const scene1 = new ScrollMagic.Scene({
 
 
 /////////////////////// Trees 2 /////////////////////
-
 const tree2Path = {
     curviness: 0.255,
     values: [
@@ -123,7 +121,6 @@ const scene2 = new ScrollMagic.Scene({
 
 
 /////////////////////// Trees 3 /////////////////////
-
 const tree3Path = {
     curviness: 0.255,
     values: [
@@ -154,7 +151,6 @@ const scene3 = new ScrollMagic.Scene({
 
 
 /////////////////////// Trees 4 /////////////////////
-
 const tree4Path = {
     curviness: 0.255,
     values: [
@@ -185,8 +181,6 @@ const scene4 = new ScrollMagic.Scene({
 
 
 ////////////////// me again ////////////////////////
-
-
 const fallPath2 = {
     curviness: 0.255,
     values: [
@@ -226,12 +220,7 @@ const scenefallPath2 = new ScrollMagic.Scene({
     .setPin('.animation1',400)
     .addTo(controllerfallPath2);
 
-// IN THE FUTURE, should maybe use .each function to
-// loop thruogh each scene and create a new ScrollMagic scene for each one 
-
-
 /////////////////////// fish /////////////////////
-
 const fishPath = {
     autoRotate: true, 
     curviness: 0.5,
@@ -265,7 +254,6 @@ const scenefishPath = new ScrollMagic.Scene({
 
 
 /////////////////////// fish2 /////////////////////
-
 const fish2Path = {
     autoRotate: true, 
     curviness: 0.55,
@@ -273,9 +261,6 @@ const fish2Path = {
         {x:0, y: 0},
         {x:500, y: 200},
         {x:3000, y: 250},
-
-
-        // {x:1000, y: 400},
     ]
 };
 
@@ -299,99 +284,40 @@ const scenefish2Path = new ScrollMagic.Scene({
     .setTween(tweenfish2Path)
     .addTo(controllerfish2Path);
 
-    
+////////////////// me again third section ////////////////////////
+const sleepPath = {
+    curviness: 0.255,
+    values: [
+        {x:0, y: 0},
+        {x:-10, y: 100},
+        {x:-20, y: 150},
+        {x:-10, y: 200},
 
 
-// /////////////////////// Trees 2 /////////////////////
+    ]
+};
 
-// const tree2Path = {
-//     curviness: 0.255,
-//     values: [
-//         {x:0, y: 0},
-//         {x:-100, y: 0},
-//     ]
-// };
+const tweensleepPath = new TimelineLite();
 
-// const tween2 = new TimelineLite();
+tweensleepPath.add(
+    TweenLite.to(".me3", 1 , {
+    //animate with bezier plugin
+     bezier: sleepPath, 
+     ease: Power1.easeInOut   
+    })
+);
 
-// tween2.add(
-//     TweenLite.to(".trees2", 1 , {
-//      bezier: tree2Path, 
-//      ease: Power1.easeInOut   
-//     })
-// );
+const controllersleepPath = new ScrollMagic.Controller();
 
-// const controller2 = new ScrollMagic.Controller();
-
-// const scene2 = new ScrollMagic.Scene({
-//     triggerElement: '.animation',
-//     duration: 5000,
-//     offset: 200,
-//     triggerHook: 0
-// } ) 
-//     .setTween(tween2)
-//     .addTo(controller2);
-
-
-// /////////////////////// Trees 3 /////////////////////
-
-// const tree3Path = {
-//     curviness: 0.255,
-//     values: [
-//         {x:800, y: 50},
-//         {x:850, y: 100},
-//     ]
-// };
-
-// const tween3 = new TimelineLite();
-
-// tween3.add(
-//     TweenLite.to(".trees3", 1 , {
-//      bezier: tree3Path, 
-//      ease: Power1.easeInOut   
-//     })
-// );
-
-// const controller3 = new ScrollMagic.Controller();
-
-// const scene3 = new ScrollMagic.Scene({
-//     triggerElement: '.animation',
-//     duration: 5000,
-//     offset: 200,
-//     triggerHook: 0
-// } ) 
-//     .setTween(tween3)
-//     .addTo(controller3);
-
-
-// /////////////////////// Trees 4 /////////////////////
-
-// const tree4Path = {
-//     curviness: 0.255,
-//     values: [
-//         {x:-800, y: 50},
-//         {x:-700, y: 100},
-//     ]
-// };
-
-// const tween4 = new TimelineLite();
-
-// tween4.add(
-//     TweenLite.to(".trees4", 1 , {
-//      bezier: tree4Path, 
-//      ease: Power1.easeInOut   
-//     })
-// );
-
-// const controller4 = new ScrollMagic.Controller();
-
-// const scene4 = new ScrollMagic.Scene({
-//     triggerElement: '.animation',
-//     duration: 5000,
-//     offset: 200,
-//     triggerHook: 0
-// } ) 
-//     .setTween(tween4)
-//     .addTo(controller4);
-
-
+const scenesleepPath = new ScrollMagic.Scene({
+    triggerElement: '.animation2',
+    duration: 5000,
+    offset: 200,
+    //when it reaches the end of the animation section, triggers 
+    triggerHook: 0
+} ) 
+    .setTween(tweensleepPath)
+    // .addIndicators()
+    //stuck here 
+    .setPin('.animation2',400)
+    .addTo(controllersleepPath);
